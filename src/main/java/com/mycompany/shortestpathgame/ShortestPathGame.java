bd6cc5655d768d6817c8c8110d0f1e60291ff78d
hansi weerathunga <hansi@avenirhealth.lk>
2023-09-07 14:08:53 +0530

    create repository

---
+++ src/main/java/com/mycompany/shortestpathgame/ShortestPathGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shortestpathgame;
+
+/**
+ *
+ * @author HansiWee
+ */
+public class ShortestPathGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
